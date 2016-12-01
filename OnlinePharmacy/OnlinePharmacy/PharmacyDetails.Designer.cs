84ee3d5d06d416b58559d872efce769c58f7c605
RichmondAsio <richmond.asio@st.vvu.edu.gh>
2016-12-01 12:01:14 +0000

    Pharmacist Entry

---
+++ OnlinePharmacy/OnlinePharmacy/PharmacyDetails.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace OnlinePharmacy
+{
+    partial class PharmacyDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPharmacyID = new System.Windows.Forms.TextBox();
+            this.txtPharmacyAddress = new System.Windows.Forms.TextBox();
+            this.txtPharmacistUsername = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtEMail = new System.Windows.Forms.TextBox();
+            this.lblPharmacyId = new System.Windows.Forms.Label();
+            this.lblPharmacyAddress = new System.Windows.Forms.Label();
+            this.lblPharmacistUsername = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblEMail = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblLastSignIn = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtPharmacyID
+            // 
+            this.txtPharmacyID.Location = new System.Drawing.Point(262, 16);
+            this.txtPharmacyID.Name = "txtPharmacyID";
+            this.txtPharmacyID.Size = new System.Drawing.Size(100, 20);
+            this.txtPharmacyID.TabIndex = 0;
+            // 
+            // txtPharmacyAddress
+            // 
+            this.txtPharmacyAddress.Location = new System.Drawing.Point(262, 55);
+            this.txtPharmacyAddress.Name = "txtPharmacyAddress";
+            this.txtPharmacyAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtPharmacyAddress.TabIndex = 1;
+            // 
+            // txtPharmacistUsername
+            // 
+            this.txtPharmacistUsername.Location = new System.Drawing.Point(262, 100);
+            this.txtPharmacistUsername.Name = "txtPharmacistUsername";
+            this.txtPharmacistUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtPharmacistUsername.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(262, 148);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(262, 192);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(100, 20);
+            this.txtMiddleName.TabIndex = 4;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(262, 248);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(100, 20);
+            this.txtLName.TabIndex = 5;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(262, 293);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtPhoneNumber.TabIndex = 6;
+            // 
+            // txtEMail
+            // 
+            this.txtEMail.Location = new System.Drawing.Point(262, 335);
+            this.txtEMail.Name = "txtEMail";
+            this.txtEMail.Size = new System.Drawing.Size(100, 20);
+            this.txtEMail.TabIndex = 7;
+            // 
+            // lblPharmacyId
+            // 
+            this.lblPharmacyId.AutoSize = true;
+            this.lblPharmacyId.Location = new System.Drawing.Point(59, 16);
+            this.lblPharmacyId.Name = "lblPharmacyId";
+            this.lblPharmacyId.Size = new System.Drawing.Size(81, 13);
+            this.lblPharmacyId.TabIndex = 8;
+            this.lblPharmacyId.Text = "PHARMACY ID";
+            // 
+            // lblPharmacyAddress
+            // 
+            this.lblPharmacyAddress.AutoSize = true;
+            this.lblPharmacyAddress.Location = new System.Drawing.Point(59, 62);
+            this.lblPharmacyAddress.Name = "lblPharmacyAddress";
+            this.lblPharmacyAddress.Size = new System.Drawing.Size(122, 13);
+            this.lblPharmacyAddress.TabIndex = 9;
+            this.lblPharmacyAddress.Text = "PHARMACY ADDRESS";
+            // 
+            // lblPharmacistUsername
+            // 
+            this.lblPharmacistUsername.AutoSize = true;
+            this.lblPharmacistUsername.Location = new System.Drawing.Point(59, 107);
+            this.lblPharmacistUsername.Name = "lblPharmacistUsername";
+            this.lblPharmacistUsername.Size = new System.Drawing.Size(141, 13);
+            this.lblPharmacistUsername.TabIndex = 10;
+            this.lblPharmacistUsername.Text = "PHARMACIST USERNAME";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(59, 155);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(72, 13);
+            this.lblFirstName.TabIndex = 11;
+            this.lblFirstName.Text = "FIRST NAME";
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Location = new System.Drawing.Point(59, 195);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(82, 13);
+            this.lblMiddleName.TabIndex = 12;
+            this.lblMiddleName.Text = "MIDDLE NAME";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(59, 248);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(68, 13);
+            this.lblLastName.TabIndex = 13;
+            this.lblLastName.Text = "LAST NAME";
+            // 
+            // lblEMail
+            // 
+            this.lblEMail.AutoSize = true;
+            this.lblEMail.Location = new System.Drawing.Point(59, 335);
+            this.lblEMail.Name = "lblEMail";
+            this.lblEMail.Size = new System.Drawing.Size(42, 13);
+            this.lblEMail.TabIndex = 14;
+            this.lblEMail.Text = "E-MAIL";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(59, 293);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(95, 13);
+            this.lblPhoneNumber.TabIndex = 15;
+            this.lblPhoneNumber.Text = "PHONE NUMBER";
+            // 
+            // lblLastSignIn
+            // 
+            this.lblLastSignIn.AutoSize = true;
+            this.lblLastSignIn.Location = new System.Drawing.Point(54, 392);
+            this.lblLastSignIn.Name = "lblLastSignIn";
+            this.lblLastSignIn.Size = new System.Drawing.Size(77, 13);
+            this.lblLastSignIn.TabIndex = 16;
+            this.lblLastSignIn.Text = "LAST SIGN-IN";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(193, 385);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // PharmacyDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 434);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblLastSignIn);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblEMail);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblPharmacistUsername);
+            this.Controls.Add(this.lblPharmacyAddress);
+            this.Controls.Add(this.lblPharmacyId);
+            this.Controls.Add(this.txtEMail);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtPharmacistUsername);
+            this.Controls.Add(this.txtPharmacyAddress);
+            this.Controls.Add(this.txtPharmacyID);
+            this.Name = "PharmacyDetails";
+            this.Text = "PharmacyDetails";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPharmacyID;
+        private System.Windows.Forms.TextBox txtPharmacyAddress;
+        private System.Windows.Forms.TextBox txtPharmacistUsername;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtEMail;
+        private System.Windows.Forms.Label lblPharmacyId;
+        private System.Windows.Forms.Label lblPharmacyAddress;
+        private System.Windows.Forms.Label lblPharmacistUsername;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblEMail;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblLastSignIn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
